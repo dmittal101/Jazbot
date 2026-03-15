69bde04b5bd5f201cb5ba01a168933d1112351c6
Dhruv Mittal <137853627+dmittal101@users.noreply.github.com>
2026-03-15 19:49:53 -0400

    patch to check.js

--- check.js
+++ check.js
@@ -1,19 +1,19 @@
 import ical from "node-ical";
 
 // Grab secret vars from github repo
-const WEBHOOK_URL = new process.env.DISCORD_WEBHOOK_URL;
-const ICAL_URL = new process.env.ICAL_URL;
+const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
+const ICAL_URL = process.env.ICAL_URL;
 const WINDOW_MINUTES = 20;
 
 const main = async () => {
     // Grabs all calendar events from google calendar
-    const events = await ical.async.fromUrl(ICAL_URL);
+    const events = await ical.async.fromURL(ICAL_URL);
     const now = new Date();
 
     for(const event of Object.values(events)){
         if(event.type !== "VEVENT") continue;
         const start = new Date(event.start);
-        const timeUntil = (start - now) / 1000 / 60;
+        const minsUntil = (start - now) / 1000 / 60;
 
         if(minsUntil >= 55 && minsUntil <= 55 + WINDOW_MINUTES){
             const timeString = start.toLocaleTimeString(
