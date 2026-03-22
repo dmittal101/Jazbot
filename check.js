aa39c2263bcb45945f3eab2f0b5b9fe5b2280d01
Dhruv Mittal <137853627+dmittal101@users.noreply.github.com>
2026-03-22 17:18:21 -0400

    Update check.js

--- check.js
+++ check.js
@@ -31,7 +31,7 @@ const main = async () => {
                     method: "POST",
                     headers: {"Content-Type": "application/json"},
                     body: JSON.stringify({
-                        content: `🕺 **Reminder for all Members!**\n` +
+                        content: `🕺 **Reminder for all Members!** @everyone\n` +
                                  `**${event.summary}** starts in about 1 hour at **${timeString}**!\n` +
                                  (event.location ? `@📍 ${event.location}` : '')
                     })
